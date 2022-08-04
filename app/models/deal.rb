3e0be208af760e84cdb7b5a8e2adb322ed13fecc
Cédric Kossi <lionel.kossi@gmail.com>
2022-08-04 10:31:35 +0000

    Update Deal model's association

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,7 @@
 class Deal < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :author, class_name: '::User', foreign_key: :author_id
   belongs_to :category
+
+  validates :name, length: { minimum: 1 }
+  validates :amount, numericality: { only_integer: true, greater_than: 0 }
 end
