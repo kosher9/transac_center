0477cc86fc324c0cb821b78580523d95b07a9956
Cédric Kossi <lionel.kossi@gmail.com>
2022-08-04 21:56:33 +0000

    Add some styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   # resources :deals
   # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "users#index"
+  root "categories#index"
 
-  resources :users do
+  # resources :users do
     resources :categories, only: [:index, :show, :new, :create] do
       resources :deals, only: [:index, :show, :new, :create]
     end
-  end
+  # end
 
 end
