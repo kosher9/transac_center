ac97110e11730cd2d01f15898f9b5491494e7ba0
Cédric Kossi <lionel.kossi@gmail.com>
2022-08-05 10:34:56 +0000

    Fix rubocop

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,18 +22,15 @@ class CategoriesController < ApplicationController
 
   # POST /categories or /categories.json
   def create
-    # @category = Category.new(category_params)
-    file_content = open(category_params['icon'].tempfile, &:read)
-    p file_content
-    # file = File.open(file_upload, 'rb') do |img|
-    #   'data:image/png;base64,' + Base64.strict_encode64(img.read)
-    # end
+    file_content = File.read(category_params['icon'].tempfile, &:read)
     encoded_string = Base64.strict_encode64(file_content)
     @category = Category.create(author: current_user, name: category_params['name'], icon: encoded_string)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to categories_path(user_id: current_user.id), notice: 'Category was successfully created.' }
+        format.html do
+          redirect_to categories_path(user_id: current_user.id), notice: 'Category was successfully created.'
+        end
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
