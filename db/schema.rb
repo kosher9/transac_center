2098c9a74b7e345c9531593cff409bdde8defad0
Cédric Kossi <lionel.kossi@gmail.com>
2022-08-05 01:09:49 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_144544) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.string "icon"
+    t.text "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
