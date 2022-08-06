bc55d23715ec56f0e30a83098e3aead98c46612b
Cédric Kossi <lionel.kossi@gmail.com>
2022-08-06 03:31:21 +0000

    Add test for deal model

---
+++ spec/models/deal_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Deal, type: :model do
+  before :each do
+    @author = User.create(name: 'Tom', email: 'wvw@gmail.com', password: 'dwefwe')
+    @category = Category.create(author: @author, icon: 'wcwrknvevnenevvk', name: 'category')
+  end
+
+  it 'is valid with valid attributes ' do
+    expect(Deal.create(author: @author, category: @category, name: 'deal', amount: 24)).to be_valid
+  end
+
+  it 'is not valid with empty name ' do
+    expect(Deal.create(author: @author, category: @category, name: '', amount: 24)).to_not be_valid
+  end
+
+  it 'is not valid with amount value 0' do
+    expect(Deal.create(author: @author, category: @category, name: 'deal', amount: 0)).to_not be_valid
+  end
+
+end
